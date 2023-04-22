419a61f09c3b7cda73e28a0b7a8eb6f96cfbaa6e
YessicaDominguez5 <100494113+YessicaDominguez5@users.noreply.github.com>
2023-04-22 18:34:26 -0300

    clase Fecha
    Constructor con fecha por defecto, validación de fecha con constructor por parámetros, mostrar fecha con el formato DD/MM/YYYY, agregar día/s, restar día/s.

---
+++ Fecha/Fecha/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Fecha.h"
+
+using namespace std;
+
+int main()
+{
+Fecha f1(1,1,2021);
+
+//cout << f1.GetAnio(); //prueba validación de días meses y años correctos
+
+//cout << f1.ToString();// prueba de formato
+
+
+//f1.AgregarDias(10); //prueba agregandole 10 dias a la fecha por parametro.
+//cout << f1.ToString();
+
+//f1.RestarDia();
+//cout << f1.ToString();
+
+f1.RestarDias(10);
+cout << f1.ToString();
+
+
+    return 0;
+}
